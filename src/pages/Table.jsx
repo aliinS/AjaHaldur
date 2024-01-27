2104e701bdd372e07125ee65a9e399178599f333
Matu16 <98150410+Matu16@users.noreply.github.com>
2024-01-27 20:22:49 +0200

    poolik

--- src/pages/Table.jsx
+++ src/pages/Table.jsx
@@ -5,11 +5,49 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { toast } from "sonner";
 import { logout } from "@/api/auth";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import * as React from "react"
+import { format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+
+
+
+
+
+
 
 export default function SingleTable() {
   const { id } = useParams();
   const [data, setData] = useState([]);
   const navigate = useNavigate();
+  const [date, setDate] = React.useState();
 
   let promise = null;
 
@@ -27,9 +65,9 @@ export default function SingleTable() {
       toast.promise(promise, {
         loading: "Loading...",
         success: (data) => {
-          return `Table created successfully`;
+          return `Table updated successfully`;
         },
-        error: "Table cannot be created",
+        error: "can't retrieve data",
       });
     });
   }, []);
@@ -99,6 +137,59 @@ export default function SingleTable() {
 
       <div className="text-[#c2c2c2] font-thin pt-6 ml-14">
         <h1 className="ml-9 text-xl">{data?.title}</h1>
+
+        <div className="flex flex-col ml-8 mt-8 gap-4">
+          <Card className='flex w-fit'>
+            <CardContent className='flex flex-col items-center gap-4 mt-4 lg:flex-row lg:mt-6'>
+              <Popover className='flex w-fit'>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant={"secondary"}
+                    className={cn(
+                      "w-11/12 px-1 justify-start text-left font-normal",
+                      !date && "text-muted-foreground"
+                    )}
+                  >
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {date ? format(date, "PPP") : <span>Pick a date</span>}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0">
+                  <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+
+              <Input className="flex w-11/12" type="number" placeholder="Hours" />
+
+              <Separator className="my-2 lg:w-[1px] lg:h-full" />
+
+              <Button className='flex w-11/12' variant="secondary">Submit</Button>
+            </CardContent>
+          </Card>
+
+          <Table className='w-11/12'>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">Date</TableHead>
+                <TableHead>Hours</TableHead>
+                <TableHead className="text-right">Object</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="font-medium">27.01.2024</TableCell>
+                <TableCell>12h</TableCell>
+                <TableCell className="text-right">Kalevi põik, 8, korter 14, elutuba</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+
+        </div>
       </div>
     </div>
   );
