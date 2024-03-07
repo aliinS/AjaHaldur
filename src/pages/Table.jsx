7145376660ac8c061e4a209457d6cf7261b9180a
Matlik-Official <lihtsaltkasuta@gmail.com>
2024-03-07 13:58:07 +0200

    Dynamyc group data fetch | Member invite | Group name update

--- src/pages/Table.jsx
+++ src/pages/Table.jsx
@@ -18,7 +18,7 @@ import {
 } from "@/components/ui/popover";
 import * as React from "react";
 import { format } from "date-fns";
-import { Calendar as CalendarIcon } from "lucide-react";
+import { Calendar as CalendarIcon, Pencil, Trash } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
