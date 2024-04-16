6a532840229b927728a0bfac5a6f30a888dd3b08
aliinS <aliin.sepp@gmail.com>
2024-04-16 11:28:32 +0300

    NotFound redesign,

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,6 +5,8 @@ import { Navigation } from "@/components/elements/navigation";
 import { Button } from "@/components/ui/button";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Link, useLocation } from "react-router-dom";
+import Navbar from "../components/elements/WelcomeNavbar";
+import Footer from "../components/elements/Footer";
 
 function Welcome() {
   // axios.get('test').then((response) => {
@@ -15,29 +17,26 @@ function Welcome() {
 
   return (
     <>
-      <div className="flex flex-col relative h-[100vh] p-[2rem] max-w-7xl mx-auto px-6">
+    <Navbar />
+      <div className="flex flex-col relative p-[2rem] max-w-7xl mx-auto px-6 bg-gradient-to-r from-bgLight gap-14 pb-28">
         {/* Development alert */}
-        <Alert>
-          <AlertTriangle color="white" className="h-4 w-4" />
+        <Alert className="mt-24 bg-bgMid">
+          <AlertTriangle color="green" className="h-4 w-4 " />
           <AlertTitle>Alert!</AlertTitle>
           <AlertDescription>
             This page is under development. Expect bugs to appear
           </AlertDescription>
         </Alert>
 
-        {/* Nav */}
-        <div className="flex justify-between items-center">
-          <img src={logo} alt="" />
-
-          <Navigation />
+        {/* page content */}
+        <div className="flex flex-col gap-4">
+          <h1 className="text-6xl text-textInLight">404 <span className="text-2xl text-textInLight"> - Page `{location.pathname}` not found</span></h1>
         </div>
-
-        {/* Homepage content */}
-        <div className="flex flex-col w-full text-gray-100 gap-4">
-          <h1 className="text-6xl">404 <span className="text-2xl"> - Page `{location.pathname}` not found</span></h1>
-          <Button variant='secondary' className='w-fit'><Link to="/">Return to Homepage</Link></Button>
-        </div>
+        <Link to="/"><Button variant='secondary' className='w-fit bg-buttonLight rounded-none text-textInLight'>Return to Homepage</Button></Link>
       </div>
+
+
+      <Footer />
     </>
   );
 }
